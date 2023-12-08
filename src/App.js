b8779a5e5cc31344b1f8b02b7fe65c2fc1fec10a
GaranceZ <zajdlicgarance@gmail.com>
2023-12-08 09:14:44 +0100

    pour pouvoir pull

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Modal from 'react-modal';
 import DevisProfessionnel from "./Views/DevisProfessionnel";
 import ReservationSite from "./Views/ReservationSite";
 import PageLocalisation from "./Views/PageLocalisation";
+import PageDetails from "./Views/PageDetails";
 
 // Définir l'élément racine de votre application pour le composant Modal
 Modal.setAppElement('#root');
@@ -19,16 +20,15 @@ function App() {
   return <>
   
     <BrowserRouter>  
-      <Header/>  
       <Routes>
-
+      <Route path={"/"} element={<PageAccueil/>} />
         <Route path={"/contact"} element={<PageContact/>} />
         <Route path={"/localisation"} element={<PageLocalisation/>} />
         <Route path={"/map"} element={<Map/>} />
         <Route path={"/devisPro"} element={<DevisProfessionnel/>} />
         <Route path={"/ReservationSite"} element={<ReservationSite/>}/>
+        <Route path={"/details"} element={<PageDetails/>}/>
       </Routes>
-      <Footer/>
     </BrowserRouter>
 
   </>
--- src/Views/PageContact.jsx
+++ src/Views/PageContact.jsx
@@ -11,6 +11,7 @@ const PageContact = () => {
     <Header/>
     <FormContact/>
     <Footer/>
+   
     
 
     </>;
