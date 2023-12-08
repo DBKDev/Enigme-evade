abc3d997286418fd1d2eabde0dfae49851148173
GaranceZ <zajdlicgarance@gmail.com>
2023-12-08 11:15:38 +0100

    modif details

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PageAccueil from "./Views/PageAccueil";
-import Map from "./Components/Map";
 import PageContact from "./Views/PageContact";
 import Modal from 'react-modal';
-import DevisProfessionnel from "./Views/DevisProfessionnel";
-import ReservationSite from "./Views/ReservationSite";
 import MonCompte from "./Views/MonCompte";
 import PageDomicile from "./Views/PageDomicile";
 import DevisProfessionnel from "./Views/DevisProfessionnel";
@@ -25,10 +22,9 @@ function App() {
   
     <BrowserRouter>  
       <Routes>
-      <Route path={"/"} element={<PageAccueil/>} />        <Route path={"/"} element={<PageAccueil/>} />
+      <Route path={"/"} element={<PageAccueil/>} />  
         <Route path={"/contact"} element={<PageContact/>} />
         <Route path={"/localisation"} element={<PageLocalisation/>} />
-        <Route path={"/map"} element={<Map/>} />
         <Route path={"/devisPro"} element={<DevisProfessionnel/>}/>
         <Route path={"/ReservationSite"} element={<ReservationSite/>}/>
         <Route path={"/details"} element={<PageDetails/>}/>
