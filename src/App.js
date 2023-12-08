22b35b3dca36cfb981385e3bf10dc520e461b872
GaranceZ <zajdlicgarance@gmail.com>
2023-12-08 11:49:13 +0100

    oui

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Modal from 'react-modal';
 import PageAccueil from "./Views/PageAccueil";
 import PageContact from "./Views/PageContact";
+import DevisProfessionnel from "./Views/DevisProfessionnel";
+import ReservationSite from "./Views/ReservationSite";
 import MonCompte from "./Views/MonCompte";
 import PageDomicile from "./Views/PageDomicile";
 import MesReservations from "./Views/MesReservations";
 import PageLocalisation from "./Views/PageLocalisation";
 import PageDetails from "./Views/PageDetails";
-import DevisProfessionnel from "./Views/DevisProfessionnel";
-import ReservationSite from "./Views/ReservationSite";
+
 
 // Définir l'élément racine de votre application pour le composant Modal
 Modal.setAppElement('#root');
