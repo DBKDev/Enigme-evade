6b259164133f92131d2920047db375412393fd32
DBK <DBKDEV@outlook.fr>
2023-12-11 20:26:44 +0100

    Formulaire Reservation Site

--- src/Views/ReservationSite.jsx
+++ src/Views/ReservationSite.jsx
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useEffect,useState } from 'react'
 import "../Styles/ReservationSite.css"
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
 import Agenda from '../Components/Agenda';
-
+import { toast } from 'react-toastify';
+import reservationsiteservice from '../Services/reservationSiteService';
 
 const ReservationSite = () => {
+    
+    const [resSite, setResSite] = useState({        
+        res_type : "site",
+        res_dateHeure: "2023-12-09 00:00:00",
+        res_nbPersonne:"",
+        res_niveau:"",
+        user_email:"",
+        res_nom:"",
+        res_prenom:"",
+        res_numero:"",
+        res_montant:"200",
+        salle_id:"2",
+    });
+
+    const handleChangeRes = (e) => {
+        const {name, value} = e.currentTarget;
+        setResSite({...resSite, [name] : value})
+        console.log(resSite)
+    }
+
+    const handleAddRes = async (e) => {
+        e.preventDefault()
+        try {
+            const response = await reservationsiteservice.AddReservationSite(resSite)
+            toast.success("Votre inscription est confirmé M." + resSite.res_nom)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
     return ( <>
+
+        
+
 
     <Header/>
     <div className='reservationSiteMargin'>
@@ -16,7 +50,7 @@ const ReservationSite = () => {
         <div className='reservationSiteContainer'>
         <form className='reservationSiteFlex'>
             <select id="escapeGameSite">
-                <option value="" disabled selected>Choisissez votre escape Game</option>
+                <option disabled selected name="salle_id" value={resSite.salle_id} onChange={handleChangeRes}>Choisissez votre escape Game</option>
                 <optgroup label="Sur Site">
                     <option value="site-1">Escape Game 1</option>
                     <option value="site-2">Escape Game 2</option>
@@ -26,20 +60,22 @@ const ReservationSite = () => {
             </select>
 
     
-            <select id="difficultySite">
-                <option value="" disabled selected>Choisissez votre difficulté</option>
+            <select id="difficultySite" name="res_niveau" value={resSite.res_niveau} onChange={handleChangeRes} >
+                <option value="" disabled selected >Choisissez votre difficulté</option>
                 <option value="facile">Facile</option>
                 <option value="intermediaire">Intermédiaire</option>
                 <option value="difficile">Difficile</option>
             </select>
-                <div className='agendaSite'>{<Agenda/>}</div>
+
+                <div className='agendaSite' name="res_date" onChange={handleChangeRes}>{<Agenda/>}</div>
             <div className='nomPrenomReservationSite'> 
-            <input type="text" placeholder='Nom' name={'nom'}/>
-            <input type="text" placeholder='Prénom' name={'prenom'}/>
+            <input type="text" placeholder='Nom' name='res_nom' value={resSite.res_nom} onChange={handleChangeRes}/>
+            <input type="text" placeholder='Prénom' name='res_prenom' value={resSite.res_prenom} onChange={handleChangeRes}/>
             </div>
-            <input type="email"  placeholder='Adresse email'/>
-            <input type="number" placeholder='Numéro de téléphone' maxLength={'10'}/>
-            <select name="" id="nbJoueurSite">
+            <input type="email"  name="user_email" placeholder='Adresse email' value={resSite.user_email} onChange={handleChangeRes}/>
+            <input type="number" name="res_numero" placeholder='Numéro de téléphone' maxLength={'10'} value={resSite.res_numero} onChange={handleChangeRes}/>
+            
+            <select name="res_nbPersonne" id="nbJoueurSite" value={resSite.res_nbPersonne} onChange={handleChangeRes}>
                 <option value="" disabled selected>Nombres de joueurs</option>
                 <option value="3">3</option>
                 <option value="4">4</option>
@@ -49,7 +85,7 @@ const ReservationSite = () => {
                 <option value="8">8</option>
             </select>
 
-            <input type="submit" value={'ENVOYER'} />
+            <input type="submit" value={'ENVOYER'} onClick={handleAddRes}/>
         </form>
         </div>
         
